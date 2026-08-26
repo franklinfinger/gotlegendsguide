02e19eb7c26d53581e654b5839b616d34f828510
Frank Finger <franklinfinger@gmail.com>
2026-08-26 15:19:09 +0200

    Add Free Cities portraits batch 1a (adolescentviserion, adolescentrhaegal)

---
+++ portraits-fc-1a.js
@@ -0,0 +1,5 @@
+var CHAMPION_PORTRAITS = CHAMPION_PORTRAITS || {};
+Object.assign(CHAMPION_PORTRAITS, {
+  "adolescentviserion": "PLACEHOLDER_WILL_FIX",
+  "adolescentrhaegal": "PLACEHOLDER_WILL_FIX"
+});
